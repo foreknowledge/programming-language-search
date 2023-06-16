501820657e69245e17a30279e8938c63ddebf8f8
foreknowledge <kyj_0129@naver.com>
2023-06-16 19:00:12 +0900

    refactor: Cache api call

--- src/api.js
+++ src/api.js
@@ -1,11 +1,19 @@
 const BASE_URL =
   'https://wr4a6p937i.execute-api.ap-northeast-2.amazonaws.com/dev';
 
+const cache = {};
+
 async function request(url) {
+  if (cache[url]) {
+    return cache[url];
+  }
+
   const res = await fetch(url);
 
   if (res.ok) {
-    return await res.json();
+    const result = await res.json();
+    cache[url] = result;
+    return result;
   }
 
   throw new Error('API 호출에 실패했습니다.');
